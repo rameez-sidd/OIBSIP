79019174dfb14b97702c7bc1ccf077d3b76e3b9a
rameez-sidd <rameezkainbox@gmail.com>
2024-07-19 11:58:34 +0530

    First commit

---
+++ Level-1-Task-1/js/script.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 91,
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('#quick-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 91,
+            behavior: 'smooth'
+        });
+    });
+});
+
